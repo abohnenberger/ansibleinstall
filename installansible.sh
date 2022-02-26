c071db452026be6e89f74e07760788dc4ed05114
abohnenberger <45307402+abohnenberger@users.noreply.github.com>
2022-02-26 04:33:01 -0500

    Update installansible.sh

--- installansible.sh
+++ installansible.sh
@@ -6,3 +6,5 @@ pip3 install pywinrm
 sudo apt install krb5-user -y
 sudo apt-get install python3-kerberos -y
 sudo pip3 install ansible
+#check installed packages
+apt list --installed | grep gcc python-dev libkrb5-dev, python3-pip, pip, python3-kerberos
